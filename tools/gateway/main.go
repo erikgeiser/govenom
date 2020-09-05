79bfdab21e7e697a9d1930ef010e59a393f2b78b
Erik G <erik.geiser@posteo.net>
2020-09-05 19:48:03 +0200

    fix: Handle payload disconnects gracefully in gateway (#5)

--- tools/gateway/main.go
+++ tools/gateway/main.go
@@ -1,15 +1,9 @@
 package gateway
 
 import (
-	"context"
-	"fmt"
-	"io"
 	"log"
-	"net"
 
-	"github.com/hashicorp/yamux"
 	"github.com/spf13/cobra"
-	"golang.org/x/sync/errgroup"
 )
 
 // Opts hold the options for the gateway command
@@ -45,78 +39,6 @@ func runGateway(opts Opts) error {
 		err := runGatwayForPayloadConnection(opts)
 		if err != nil {
 			log.Printf("gateway error: %v", err)
-		} else {
-			fmt.Println("payload disconnected")
 		}
-
-		fmt.Println("waiting for new payload connection")
 	}
-}
-
-func runGatwayForPayloadConnection(opts Opts) error {
-	listener, err := net.Listen("tcp", opts.connectBackAddress)
-	if err != nil {
-		return fmt.Errorf("listen: %w", err)
-	}
-
-	log.Printf("listening for payload connection on tcp/%s", opts.connectBackAddress)
-
-	targetConn, err := listener.Accept()
-	if err != nil {
-		return fmt.Errorf("accept: %w", err)
-	}
-
-	log.Printf("received connection from %s", targetConn.RemoteAddr())
-
-	client, err := yamux.Client(targetConn, nil)
-	if err != nil {
-		return fmt.Errorf("connection multiplexer setup: %w", err)
-	}
-
-	return startGateway(opts.gatewayAddress, client)
-}
-
-func startGateway(gatewayAddress string, sess *yamux.Session) error {
-	gateway, err := net.Listen("tcp", gatewayAddress)
-	if err != nil {
-		return fmt.Errorf("listen: %w", err)
-	}
-
-	log.Printf("start socks5 gateway on tcp/%s", gatewayAddress)
-
-	for {
-		conn, err := gateway.Accept()
-		if err != nil {
-			return fmt.Errorf("accept: %w", err)
-		}
-
-		go func() {
-			err := handleGatewayConn(conn, sess)
-			if err != nil {
-				log.Printf("error while handling gateway connection: %v", err)
-			}
-		}()
-	}
-}
-
-func handleGatewayConn(conn net.Conn, sess *yamux.Session) error {
-	yamuxConn, err := sess.Open()
-	if err != nil {
-		return fmt.Errorf("open multiplexed connection: %w", err)
-	}
-
-	log.Print("multiplexed connection opened")
-
-	eg, _ := errgroup.WithContext(context.Background())
-
-	eg.Go(func() error {
-		_, err := io.Copy(yamuxConn, conn)
-		return err
-	})
-	eg.Go(func() error {
-		_, err := io.Copy(conn, yamuxConn)
-		return err
-	})
-
-	return eg.Wait()
 }
